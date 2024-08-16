360c16b367de1c089bfc7ffa93ea368c3a3ffcf6
Ayush Nepali <anepali818@gmail.com>
2024-08-16 21:59:32 +0545

    Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="flex max-h-screen flex-col items-center justify-between p-10">
       <div className={silkscreen.className}>
-        <h1 className="text-4xl mb-4">Flappy Bird</h1>
+        <h1 className="text-5xl mb-4">Flappy Bird</h1>
       </div>
       <FlappyBird />
     </main>
